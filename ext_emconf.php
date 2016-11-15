e568526b2c201b70e3c3adc93b55715f9242d501
PIT Solutions Private Limited <pitsolutionstypo3@users.noreply.github.com>
2016-11-15 14:11:21 +0530

    Fixed Invalid Version Tags

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.x-8.4.x',
+            'typo3' => '6.2.0-8.4.99',
         ),
         'conflicts' => array(
         ),
